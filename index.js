8adbc7b64a10c3fbd79953f8a0bc7bcdc10dfb9f
Mike Gorton <mike.gorton1@gmail.com>
2019-01-19 18:15:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+var array = [];
+
+function theBeatlesPlay (mus, inst) {
+  for (var i = 0; i < 4; i++) {
+    array.push(`${mus[i]} plays ${inst[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while(i != facts.length) {
+    var selectElement = facts.splice(facts[i], 1, `${facts[i]}!!!`);
+    array.push(selectElement);
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(num) {
+  var array2 = [];
+  do {
+    var pushTimes = 15 - num;
+    array2.push("I love the Beatles!") * pushTimes;
+    num++;
+  } while (num < 15);
+  return array2;
+}
